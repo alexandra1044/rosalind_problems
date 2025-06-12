1e222868150ab2d68129ae241044d3ccd5b949db
Alexandra Cooper <a.cooper8789@gmail.com>
2025-06-12 17:35:42 +0100

    more problems

--- Mendel's_First_Law.py
+++ Mendel's_First_Law.py
@@ -1,2 +1,12 @@
-a, b, c = (map(int, input()))
+a, b, c = (map(int, input().split()))
 
+k = float(a) # homozygous dominant
+m = float(b) # heterozygous
+n = float(c) # homozygous recessive
+
+population_size = k + m + n
+
+p_rr = (n*(n-1) + n*m + 0.25*m*(m-1))/(population_size*(population_size-1))
+p_rr = round(1 - p_rr, 5)
+
+print(p_rr)
